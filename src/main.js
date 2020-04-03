90a41c2999066d7f32f00c549e5ed486844f3e66
Adilet312 <adiletm@hotmail.com>
2020-04-03 14:43:59 -0700

    Add Api call based on album's id to get all photos.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
+                   /*Get albums*/
 document.addEventListener('DOMContentLoaded', () => {
   const generateAlbums = document.getElementById('js-albums');
   generateAlbums.addEventListener('click', getAllAlbums);
-
 });
+
 
 let getAllAlbums = () =>
 {
@@ -41,4 +41,33 @@ let getAlbums = (albums) =>{
     idx = idx + 1;
   }
 
+}
+
+                        /*Get Photos based on given album Id*/
+  document.addEventListener('DOMContentLoaded', () => {
+     const generatePhotos = document.getElementById('js-photos');
+     generatePhotos.addEventListener('click', getAllPhotos);
+     });
+
+  let getAllPhotos = () => {
+    let albumId = document.getElementById('inputId').value;
+    console.log(albumId);
+    fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
+    .then((data) => data.json())
+    .then((data) => getPhotos(data));
+
+}
+
+let getPhotos = (photos) => {
+  let ul = document.getElementById('photos');
+  for(let idx=0; idx<photos.length; idx++)
+    {
+      let picture = photos[idx];
+      let li = document.createElement('li');
+      let imgTag = document.createElement('img');
+      imgTag.src = picture.thumbnailUrl;
+      li.appendChild(imgTag);
+      ul.appendChild(li);
+
+    }
 }
